c1791ced34d2a939675fda8af4c22bd8cff9a008
Alan Ngo <u3556436@connect.hku.hk>
2019-04-09 02:33:46 +0800

    Outline

--- main.cpp
+++ main.cpp
@@ -12,33 +12,81 @@ Features of the Program
 5. Automatic change of inventory status according to the amount of commodity that is currently available (e.g. when the amount reaches zero, the inventory status would become "out-of-stock")
 6. Deliver alerts when the commodity is "out-of-stock"
 */
+void options();
+void help(string option);
+void intro();
 
 int main(){
-    string input, option;
-    istringstream inputline(input);
-    while (inputline >> option){
-        cout << "Please input your option: \nType \"options\" to view what are the options.\nType \"exit\" if you want to quit the program";
-        switch (option){
-            case ("options"){
-                options();
+    string option;
+    intro();
+    while (getline(cin, option)){
+        // change() <- which include both point 5 and 6, where update the commodity information every time before
+        if (option == "options"){
+            cout << endl;
+            options();
+            intro();
+        }
+        else{
+            if (option == "help"){
+            cout << "Which option do you to know more?" << endl;
+            cin >> option;
+            help(option);
+            intro();
             }
-            case ("help"){
-
-            }
-            case ("exit"){
+        else{
+            if (option == "exit"){
                 cout << "This is the end of the program." << endl;
-                return 0;
+                break;
+            }
+        // More options insect here
+        else{
+            cout << "Undefined option, please enter the option again." << endl << endl;
+            intro();
             }
         }
     }
 }
-// to view the options
+
+}
+// to view the options (two lines view all options)
 void options(){
-    cout << "Search" <<
+    cout << "search" << ' ' << "insect" << ' ' << "delete" << ' ' << "exit" << endl
+    << "options" << ' ' << "help" << ' ' << "exit" << endl << endl;
+    // may use a line to replace endl ???
 }
 
-//
-void help(string){
-    cout << "Which option do you to know more?" << endl;
-    
+// view options explaination
+void help(string option){
+
+    if (option == "options"){
+        cout << "To view all options." << endl;
+    }
+    else{if (option == "help"){
+        cout << "To view options explaination and guideline." << endl;
+    }
+    else{if (option == " search"){
+        cout << "To search commodities according to different filters (e.g. in-stock/out-of-stock)." << endl;
+    }
+    else{if (option == "insect"){
+        cout << "To insert new commodity with basic information (name, manufacturer, amount, price, availability of shop(s)) after each procurement." << endl;
+    }
+    else{if (option == "delete"){
+        cout << "To delete obsolete commodities." << endl;
+    }
+    else{if (option == "exit"){
+        cout << "To exit the program." << endl;
+    }
+    else{
+        cout << "This is not an option." << endl;
+    }
+}
+}
+}
+}
+}
+}
+
+void intro(){
+    cout << "Please input your option: \nType \"options\" to view what are the options and \"help\" to see further explaination of the options."
+    << "\nType \"exit\" if you want to quit the program.\n";
 }
