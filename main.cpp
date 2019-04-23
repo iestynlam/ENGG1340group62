727dfb3a9cefea96ef1d36bf8ae7398c24d32646
iestynlam <48487929+iestynlam@users.noreply.github.com>
2019-04-23 17:29:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,92 +1,106 @@
+// main but designed by me lmfao
 #include <iostream>
 #include <string>
-#include <sstream>
 using namespace std;
 
 /*
-Features of the Program
-1. Search commodities according to different filters (e.g. in-stock/out-of-stock)
-2. Insert new commodity with basic information (name, manufacturer, amount, price, availability of shop(s)) after each procurement
-3. Delete obsolete commodities
-4. Update commodity information
-5. Automatic change of inventory status according to the amount of commodity that is currently available (e.g. when the amount reaches zero, the inventory status would become "out-of-stock")
-6. Deliver alerts when the commodity is "out-of-stock"
+Program Features:
+- search commodities
+- insert new commodity
+- delete obsolete commodities
+- update commodity information (allow multipele)
+- automatic change of inventory status
+- deliver alerts when commodity is out of stock (error handling for too much minus)
+- overflow alerts?
+- stats on rate of consumption?
 */
-void options();
-void help(string option);
-void intro();
 
-int main(){
-    string option;
-    intro();
-    while (getline(cin, option)){
-        // change() <- which include both point 5 and 6, where update the commodity information every time before
-        if (option == "options"){
-            cout << endl;
-            options();
-            intro();
-        }
-        else{
-            if (option == "help"){
-            cout << "Which option do you to know more about?" << endl;
-            cin >> option;
-            help(option);
-            intro();
-            }
-        else{
-            if (option == "exit"){
-                cout << "This is the end of the program." << endl;
-                break;
-            }
-        // More options insect here
-        else{
-            cout << "Undefined option, please re-enter a valid option." << endl << endl;
-            intro();
-            }
-        }
-    }
+// Struct used to hold details of all commodities.
+struct commodity {
+  string name; // name of commodity
+  int qty; // good quantity
+} ;
+
+void intro(int x) {
+  if (x == 0) { // first intro message
+    cout << "// Welcome to the commodity manager! //" << endl << "\nType \"options\" to view available "
+    "actions and \"help\" for further details regarding available options. Type \"exit\" "
+    "to shut down the program." << endl;
+  }
+  cout << endl << "PLEASE INPUT YOUR SELECTED OPTION TO PROCEED: " << endl;
 }
 
+// INPUT : A command the user wishes to inquire further about
+// OUTPUT : Gives further detail on the function and input style of the command.
+void help(string str) {
+  if (str == "options") {
+    cout << "To view all available user commands.";
+  }
+  else if (str == "search") {
+    cout << "Used to search for commodities according to specific filters." << endl
+    << "Available filters are:" << endl << "filter 1, filter 2, etc";
+  }
+  else if (str == "insert") {
+    cout << "To insert a new commodity into the program.";
+  }
+  else if (str == "delete") {
+    cout << "To delete a commodity from the program.";
+  }
+  else if (str == "exit") {
+    cout << "Terminates the program.";
+  }
+  else if (str == "display") {
+    cout << "Displays all items currently in the system, along with"
+    " associated information.";
+  }
+  else if (str == "help") {
+    cout << "Provides further details on the functions of available commands.";
+  }
+  else {
+    cout << str << " is not a valid command.";
+  }
+  cout << endl;
 }
-// to view the options (two lines view all options)
-void options(){
-    cout << "search" << ' ' << "insect" << ' ' << "delete" << ' ' << "exit" << endl
-    << "options" << ' ' << "help" << ' ' << "exit" << endl << endl;
-    // may use a line to replace endl ???
-}
-
-// view options explaination
-void help(string option){
-
-    if (option == "options"){
-        cout << "To view all availabe options." << endl;
-    }
-    else{if (option == "help"){
-        cout << "To view options explaination and guidelines." << endl;
-    }
-    else{if (option == " search"){
-        cout << "To search commodities according to different filters (e.g. in-stock/out-of-stock)." << endl;
-    }
-    else{if (option == "insect"){
-        cout << "To insert new commodity with basic information (name, manufacturer, amount, price, availability of shop(s)) after each procurement." << endl;
-    }
-    else{if (option == "delete"){
-        cout << "To delete obsolete commodities." << endl;
-    }
-    else{if (option == "exit"){
-        cout << "To exit the program." << endl;
-    }
-    else{
-        cout << "This is not an existing option." << endl;
-    }
-}
-}
-}
-}
-}
+// Displays all available commands to the user.
+void options() {
+  cout << "Available commands are:\nsearch, insert, delete, \n"
+  "display, options, help, exit." << endl << endl;
 }
 
-void intro(){
-    cout << "Please input your option: \nType \"options\" to view what are the options and \"help\" to see further explaination of the options."
-    << "\nType \"exit\" if you want to quit the program.\n";
+// Makes the input string all lowercase characters
+// Error handling in case of wrong user input
+void lowercase(string &str) {
+  for (int i = 0; i < str.length(); i++) {
+    str[i] = tolower(str[i]);
+  }
+}
+
+int main() {
+  string option;
+  intro(0);
+
+  while (getline(cin,option)) {
+    lowercase(option);
+    cout << option << endl;
+    if (option == "options") {
+      cout << endl;
+      options();
+    }
+    else if (option == "help") {
+      cout << "Which option would you like further details on?" << endl;
+      cin >> option;
+      lowercase(option);
+      help(option);
+      continue;
+    }
+    //rest of the functions insert here
+
+    else if (option == "exit") {
+      cout << endl << "// Thank you for using the program, this program has now ended. //" << endl;
+      break;
+    }
+    intro(1);
+  }
+
+  return 0;
 }
