97399e5a8e1c37391c62bb15675b7222275c3fd8
Iestyn Lam <iestyn@kristys-mbp-2.mynet>
2019-04-24 15:16:40 +0800

    Delete does_inventory_exist.cpp

--- does_inventory_exist.cpp
+++
@@ -1,6 +0,0 @@
-// Checks whether 'inventory.txt' exists/has any items in it
-// If not then no list is initialized from the empty list? or wait actually
-
-#include <iostream>
-#include <fstream>
-using namespace std;
